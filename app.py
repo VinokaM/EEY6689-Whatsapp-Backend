04e1b29ffcf0d3a8e291a9c9c55cf282c5588955
VinokaM <s92068703@ousl.lk>
2025-12-31 00:29:36 +0530

    Initial whatsapp Flask backend

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Backend is running bro"
+    })
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
